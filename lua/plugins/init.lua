227f4a5f0d543aff8f5ee5fa077d90311d2e881a
Wei Su <cs.akasuv@gmail.com>
2022-06-04 20:07:40 +0800

    add startify

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,6 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 
+	use 'mhinz/vim-startify'
 	use 'sbdchd/neoformat'
 	use 'github/copilot.vim'
 	use 'wbthomason/packer.nvim'
